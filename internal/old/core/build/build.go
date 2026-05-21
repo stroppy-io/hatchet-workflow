40fbc49a9acb50c2fa55837ad5fd088183b19160
yaroher <stolbunov.yaroslav@gmail.com>
2026-05-21 18:43:32 +0300

    proto: start6

---
+++ internal/old/core/build/build.go
@@ -0,0 +1,13 @@
+package build
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+var (
+	Version          = "0.0.0" //nolint: gochecknoglobals // This file could be global
+	ServiceName      = ""      //nolint: gochecknoglobals // This file could be global
+	GlobalInstanceId = fmt.Sprintf("%s-%s-%s", ServiceName, Version, uuid.NewString())
+)
