3d6f0d05ef78d687ed3b012c867e4af369540e6e
yaroher <stolbunov.yaroslav@gmail.com>
2026-06-26 14:10:29 +0300

    feat(orioledb): master+replicas+haproxy topology

--- internal/domain/database/orioledb/orioledb_test.go
+++ internal/domain/database/orioledb/orioledb_test.go
@@ -8,6 +8,32 @@ import (
 
 var dummyParams = domain.OrioledbParams{}
 
+func TestBuildTopologySpecScaled(t *testing.T) {
+	spec, err := (&Database{}).BuildTopologySpec(&domain.OrioledbParams{Replicas: 2, Haproxy: 1})
+	if err != nil {
+		t.Fatalf("BuildTopologySpec: %v", err)
+	}
+	roles := map[string]int{}
+	for _, c := range spec.GetComponents() {
+		roles[c.GetRole()]++
+	}
+	if roles["master"] != 1 || roles["replica"] != 2 || roles["haproxy"] != 1 {
+		t.Fatalf("want 1 master/2 replica/1 haproxy, got %v", roles)
+	}
+	if len(spec.GetNodes()) != 4 {
+		t.Fatalf("want 4 nodes, got %d", len(spec.GetNodes()))
+	}
+	reps := 0
+	for _, cn := range spec.GetConnections() {
+		if cn.GetToComponentId() == "orioledb-master-1" && cn.GetEndpointName() == "rep" {
+			reps++
+		}
+	}
+	if reps != 2 {
+		t.Fatalf("want 2 replica->master streaming connections, got %d", reps)
+	}
+}
+
 func TestBuildTopologySpecSingleNode(t *testing.T) {
 	spec, err := (&Database{}).BuildTopologySpec(&dummyParams)
 	if err != nil {
